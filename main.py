82e13b1f3c6c4c448533bfd356aac07de7f04f2f
Jordy0481 <jvelst2009@gmail.com>
2025-08-28 16:49:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ intents.guilds = True
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
