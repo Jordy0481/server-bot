4784c301e753608412a72125904459ea4b951df0
Jordy0481 <jvelst2009@gmail.com>
2026-01-22 15:54:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -235,4 +235,4 @@ async def on_ready():
                     await guild.create_voice_channel(ch["name"], category=category)
                     print(f"Voice channel created: {ch['name']}")
 
-bot.run("DISCORD_TOKEN")
+os.getenv("DISCORD_TOKEN")
